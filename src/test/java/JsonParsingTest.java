c3340792695579a6f6aa442505eda6fc556d9a86
elik00 <elelelelik0@gmail.com>
2023-01-18 22:42:50 +0300

    JsonParsingTest

---
+++ src/test/java/JsonParsingTest.java
@@ -0,0 +1,17 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.Test;
+
+public class JsonParsingTest {
+
+    @Test
+    public void jsonParsingTest() {
+        JsonPath response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+
+        String message = response.get("messages[1].message");
+        System.out.println(message);
+
+    }
+}
