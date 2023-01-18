9c39909549930f0fe23b1f5e6e48559142d787b9
elik00 <elelelelik0@gmail.com>
2023-01-18 23:40:18 +0300

    RedirectCycleTest

---
+++ src/test/java/RedirectCycleTest.java
@@ -0,0 +1,35 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class RedirectCycleTest {
+
+    @Test
+    public void redirectTest() {
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+
+        int count = 0;
+        int statusCode = response.getStatusCode();
+
+        while (statusCode != 200) {
+            String locationHeader = response.getHeader("Location");
+            response = RestAssured
+                    .given()
+                    .redirects()
+                    .follow(false)
+                    .when()
+                    .get(locationHeader)
+                    .andReturn();
+            statusCode = response.getStatusCode();
+            count = count + 1;
+            System.out.println(locationHeader);
+        }
+        System.out.println(count);
+    }
+}
