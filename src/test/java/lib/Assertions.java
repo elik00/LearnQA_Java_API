dd044b0e596737202b3596db1f45f172d5f61dc3
elik00 <elelelelik0@gmail.com>
2023-01-23 10:27:15 +0300

    UserGetTest

--- src/test/java/lib/Assertions.java
+++ src/test/java/lib/Assertions.java
@@ -50,4 +50,10 @@ public class Assertions {
     public static void assertJsonHasNotField(Response Response, String unexpectedFieldName) {
         Response.then().assertThat().body("$", not(hasKey(unexpectedFieldName)));
     }
+
+    public static void assertJsonHasNotFields(Response Response, String[] unexpectedFieldNames) {
+        for (String unexpectedFieldName : unexpectedFieldNames) {
+            Assertions.assertJsonHasNotField(Response, unexpectedFieldName);
+        }
+    }
 }
